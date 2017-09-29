31a62eedb238ec128ada36ed877818572348cc18
sensei100 <tuh42082@temple.edu>
2017-09-29 11:50:55 -0400

    Update initializer

--- config/initializers/blacklight_searcher_config.rb
+++ config/initializers/blacklight_searcher_config.rb
@@ -3,6 +3,6 @@ if File.exists?(File.join(Rails.root, "/config/searchers/blacklight_config.yml")
   config_file = File.join Rails.root, "/config/searchers/blacklight_config.yml"
 else
   # otherwise load the default config file
-  config_file = File.expand_path("../../blacklight_config.yml", __FILE__)
+  config_file = File.expand_path("../../blacklight_searcher_config.yml", __FILE__)
 end
 QuickSearch::Engine::BLACKLIGHT_CONFIG = YAML.load_file(config_file)[Rails.env]
